2fad0600a1e92f27d6838979eb3ead643a475202
Camibaza <bazante_mila@hotmail.com>
2021-12-13 21:14:36 -0300

    Implementação do put e delete de postagens

--- src/app/service/postagem.service.ts
+++ src/app/service/postagem.service.ts
@@ -26,8 +26,20 @@ getAllPostagem(): Observable<postagem[]>{
   return this.http.get<postagem[]>("https:///blogpessoalcami.herokuapp.com/postagens", this.token)
 }
 
+getByIdPostagem(id: number): Observable<postagem>{
+  return this.http.get<postagem>(`https:///blogpessoalcami.herokuapp.com/postagens/${id}`, this.token)
+}
+
 postPostagem(postagem: postagem): Observable<postagem>{
   return this.http.post<postagem>("https:///blogpessoalcami.herokuapp.com/postagens", postagem, this.token)
+}
+
+putPostagem(postagem: postagem): Observable<postagem>{
+  return this.http.put<postagem>(`https:///blogpessoalcami.herokuapp.com/postagens`, postagem, this.token)
+}
+
+deletePostagem(id: number){
+  return this.http.delete(`https:///blogpessoalcami.herokuapp.com/postagens/${id}`, this.token)
 }
 
 }
